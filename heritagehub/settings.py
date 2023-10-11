b98cb2c314c28e6621d7780b1f7e8b25323428c5
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2023-10-11 16:13:12 +0200

    install djgo rest framwork jwt database generation

--- heritagehub/settings.py
+++ heritagehub/settings.py
@@ -132,3 +132,21 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ]
 }
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=60),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
--- heritagehub/users/models.py
+++ heritagehub/users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class usersModel(models.Model):
     nom = models.CharField(max_length=255)
-    prenom = models.TextField(ax_length=255)
+    prenom = models.TextField(max_length=255)
     age = models.IntegerField()
     date_naissance = models.DateField()
     lieu_naissance = models.CharField(max_length=255)
