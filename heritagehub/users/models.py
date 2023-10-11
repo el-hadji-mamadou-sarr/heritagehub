816883ea9c1fba5a828000dd91be03f614fde6e2
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2023-10-11 16:38:30 +0200

    Ok

---
+++ heritagehub/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class usersModel(models.Model):
+    nom = models.CharField(max_length=255)
+    prenom = models.TextField(max_length=255)
+    age = models.IntegerField()
+    date_naissance = models.DateField()
+    lieu_naissance = models.CharField(max_length=255)
+    date_de_deces = models.DateField(null=True)
+    lieu_de_deces = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom_de_famille}"
