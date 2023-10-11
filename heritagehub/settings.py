8bb7c979345dc08fa63d702c6b51818208b34253
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2023-10-11 15:57:10 +0200

    updating models

--- heritagehub/settings.py
+++ heritagehub/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'heritagehub.heritagehub',
+    'heritagehub.users',
     'rest_framework_simplejwt',
 ]
 
--- heritagehub/users/models.py
+++ heritagehub/users/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
+class usersModel(models.Model):
     nom = models.CharField(max_length=255)
     prenom = models.TextField(ax_length=255)
     age = models.IntegerField()
-    
+    date_naissance = models.DateField()
+    lieu_naissance = models.CharField(max_length=255)
+    date_de_deces = models.DateField(null=True)
+    lieu_de_deces = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return self.nom
+        return f"{self.prenom} {self.nom_de_famille}"
