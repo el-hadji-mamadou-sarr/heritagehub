6e0fd6179efc9e2535c10e206b3bea3ee8035857
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2023-10-17 18:37:41 +0200

    delete users folder

--- heritagehub/users/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import usersModel
-from .serializers import serializers
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = usersModel
-        fields = ('id','nom', 'prenom', 'age','date_naissance', 'lieu_naissance','date_de_deces',
-                  'lieu_de_deces','created_at','updated_at')
