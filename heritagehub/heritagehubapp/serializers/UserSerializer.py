2924d2e56e48aec7bc808bde156d1435ca77b175
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2023-10-20 18:09:53 +0200

    add events, famillies, marriages, persons, relations field to UserSerializer

--- heritagehub/heritagehubapp/serializers/UserSerializer.py
+++ heritagehub/heritagehubapp/serializers/UserSerializer.py
@@ -1,12 +1,18 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .FamillySerializer import FamillySerializer
-from django.contrib.auth.models import User
+from .EventSerializer import EventSerializer
+from .MarriageSerializer import MarriageSerializer
+from .PersonSerializer import PersonSerializer
+from .RelationSerializer import RelationSerializer
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     famillies = FamillySerializer(many=True,read_only=True)
-    created_by = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-
+    events = EventSerializer(many=True,read_only=True)
+    marriages = MarriageSerializer(many=True,read_only=True)
+    persons = PersonSerializer(many=True,read_only=True)
+    relations = RelationSerializer(many=True,read_only=True)
+    
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups', 'famillies']
