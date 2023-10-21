5049ac3bbdfc05a6dac69d136327b5bcdca3ef2b
el hadji mamadou sarr <elhadji.sarr@outlook.com>
2023-10-21 11:16:05 +0200

    update user serializer

--- heritagehub/heritagehubapp/serializers/UserSerializer.py
+++ heritagehub/heritagehubapp/serializers/UserSerializer.py
@@ -6,13 +6,21 @@ from .MarriageSerializer import MarriageSerializer
 from .PersonSerializer import PersonSerializer
 from .RelationSerializer import RelationSerializer
 
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    famillies = FamillySerializer(many=True,read_only=True)
-    events = EventSerializer(many=True,read_only=True)
-    marriages = MarriageSerializer(many=True,read_only=True)
-    persons = PersonSerializer(many=True,read_only=True)
-    relations = RelationSerializer(many=True,read_only=True)
-    
+    famillies = FamillySerializer(many=True, read_only=True)
+    events = EventSerializer(many=True, read_only=True)
+    marriages = MarriageSerializer(many=True, read_only=True)
+    persons = PersonSerializer(many=True, read_only=True)
+    relations = RelationSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups', 'famillies']
+        fields = ('url', 'username', 'email', 'groups', 'famillies',
+                  'events', 'marriages', 'persons', 'relations')
+
+    def __init__(self, *args, **kwargs):
+        # Pass the 'request' from the kwargs to the context
+        request = kwargs.pop('context', {}).get('request')
+        super(UserSerializer, self).__init__(
+            *args, **kwargs, context={'request': request})
--- heritagehub/heritagehubapp/views/UserViewSet.py
+++ heritagehub/heritagehubapp/views/UserViewSet.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from heritagehub.heritagehubapp.permissions import IsGetRequest,CanCreateUser,CanListUsers
+from heritagehub.heritagehubapp.permissions import IsGetRequest, CanCreateUser, CanListUsers
 from rest_framework.permissions import IsAuthenticated
 from heritagehub.heritagehubapp.serializers.UserSerializer import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -11,23 +11,21 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from django.contrib.auth.hashers import make_password
 
+
 class UserViewSet(viewsets.ModelViewSet):
-  
+
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes=[]
-    
+    permission_classes = []
+
     def get_permissions(self):
-        if self.action =='create':
+        if self.action == 'create':
             permission_class = [CanCreateUser]
-        if self.action == 'list':
+        else:
             permission_class = [CanListUsers]
 
-        else:
-            permission_class = []
-
         return [permission() for permission in permission_class]
-    
+
     @swagger_auto_schema(
         operation_description='Create a user with JWT token generation',
         request_body=openapi.Schema(
@@ -57,10 +55,11 @@ class UserViewSet(viewsets.ModelViewSet):
         # Check if a user with the same username already exists
         if User.objects.filter(username=username).exists():
             return Response({'message': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        
+
         hashed_password = make_password(password)
-        user = User.objects.create(username=username, password=hashed_password, email=email)
-        
+        user = User.objects.create(
+            username=username, password=hashed_password, email=email)
+
         # Generate JWT tokens
         refresh = RefreshToken.for_user(user)
         access_token = str(refresh.access_token)
@@ -72,13 +71,13 @@ class UserViewSet(viewsets.ModelViewSet):
             'refresh_token': str(refresh),
         }
         return Response(response_data, status=status.HTTP_201_CREATED)
-    
+
     @swagger_auto_schema(
         operation_description='List all users',
         responses={200: 'User list', 403: 'Forbidden'},
     )
     def list(self, request, *args, **kwargs):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        serializer = UserSerializer(
+            users, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
